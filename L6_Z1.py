c819b1f3883bb0f9ef3f04c439c068565990edd9
Karol Cieślik <126989110+MrKarol1995@users.noreply.github.com>
2024-05-16 19:29:20 +0200

    Update L6_Z1.py

--- L6_Z1.py
+++ L6_Z1.py
@@ -1,37 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from typing import List
 
 
-def thinning(lambda_max, T):
+def thinning(lambda_max: float, T: float) -> List[float]:
     """
     Generuje niejednorodny proces Poissona na przedziale [0, T] za pomocą metody przerzedzeń.
+
+    Parameters:
+        lambda_max (float): Maksymalna wartość funkcji intensywności.
+        T (float): Czas końcowy.
+
+    Returns:
+        List[float]: Lista zawierająca czasy przyjścia zdarzeń.
     """
-    arrivals = []
+    S = []
     t = 0
-    while t < T:
-        u1 = np.random.rand()
-        u2 = np.random.rand()
-        inter_arrival_time = -np.log(u1) / lambda_max
-        t += inter_arrival_time
-        if t < T:
-            if u2 < lambda_t(t) / lambda_max:
-                arrivals.append(t)
-    return np.array(arrivals)
-
-
-def lambda_t(t):
-    """
-    Funkcja intensywności.
-    """
-    return 5 + 0.5 * np.sin(2*np.pi*t/24)
+    while (t := t - np.log(np.random.uniform()) / lambda_max) <= T:
+        S.append(t)
+    return S
 
 
 T = 20
-arrivals = thinning(lambda_max=0.8, T=T)
+arr = thinning(lambda_max=0.8, T=T)
 
 # Wykres procesu Poissona
 plt.figure(figsize=(10, 5))
-plt.step(arrivals, range(len(arrivals)))
+plt.step(arr, range(len(arr)))
 plt.xlabel("Czas")
 plt.ylabel("Liczba przybyć")
 plt.title("Niejednorodny proces Poissona (metoda przerzedzeń)")
