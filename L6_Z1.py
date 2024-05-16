5589d5fbfb93679da3363501539176b4149086a7
Karol Cieślik <126989110+MrKarol1995@users.noreply.github.com>
2024-05-16 19:47:55 +0200

    Update L6_Z1.py

--- L6_Z1.py
+++ L6_Z1.py
@@ -1,13 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from typing import List
+from scipy.integrate import quad
 
 
-def thinning(lambda_max: float, T: float) -> List[float]:
+def thinning(lambda_t, lambda_max: float, T: float) -> List[float]:
     """
     Generuje niejednorodny proces Poissona na przedziale [0, T] za pomocą metody przerzedzeń.
 
     Parameters:
+        lambda_t (callable): Funkcja intensywności w zależności od czasu.
         lambda_max (float): Maksymalna wartość funkcji intensywności.
         T (float): Czas końcowy.
 
@@ -17,12 +19,73 @@ def thinning(lambda_max: float, T: float) -> List[float]:
     S = []
     t = 0
     while (t := t - np.log(np.random.uniform()) / lambda_max) <= T:
-        S.append(t)
+        u = np.random.uniform()
+        if u < lambda_t(t) / lambda_max:
+            S.append(t)
     return S
 
 
+def lambda_t(t: float) -> float:
+    """
+    Funkcja intensywności.
+
+    Parameters:
+        t (float): Czas.
+
+    Returns:
+        float: Wartość funkcji intensywności w punkcie czasowym t.
+    """
+    return 5 + 0.5 * np.sin(2 * np.pi * t / 24)
+
+
+def mean_arrivals(lambda_t, T: float) -> float:
+    """
+    Oblicza średnią liczbę przyjść dla niejednorodnego procesu Poissona.
+
+    Parameters:
+        lambda_t (callable): Funkcja intensywności w zależności od czasu.
+        T (float): Czas końcowy.
+
+    Returns:
+        float: Średnia liczba przyjść.
+    """
+    result, _ = quad(lambda_t, 0, T)
+    return result / T
+
+
+def mean_time_at_T(
+    lambda_t, lambda_max: float, T: float, num_realizations: int
+) -> float:
+    """
+    Oblicza średnią wartość czasu w chwili T dla 1000 realizacji niejednorodnego procesu Poissona.
+
+    Parameters:
+        lambda_t (callable): Funkcja intensywności w zależności od czasu.
+        lambda_max (float): Maksymalna wartość funkcji intensywności.
+        T (float): Czas końcowy.
+        num_realizations (int): Liczba realizacji.
+
+    Returns:
+        float: Średnia wartość czasu w chwili T.
+    """
+    times_at_T = []
+    for _ in range(num_realizations):
+        arrivals = thinning(lambda_t, lambda_max, T)
+        if arrivals:
+            times_at_T.append(arrivals[-1])
+    return np.mean(times_at_T)
+
+
 T = 20
-arr = thinning(lambda_max=0.8, T=T)
+lambda_max = np.max([lambda_t(t) for t in np.linspace(0, T, 1000)])
+mean_integrated_value = mean_arrivals(lambda_t, T)
+arr = thinning(lambda_t, lambda_max, T)
+num_realizations = 1000
+mean_time_value = mean_time_at_T(lambda_t, lambda_max, T, num_realizations)
+mean_arrivals_value = mean_arrivals(lambda_t, T)
+
+print("Średnia wartość czasu w chwili T (całka):", mean_integrated_value)
+print("Średnia wartość czasu w chwili T (1000 realizacji):", mean_time_value)
 
 # Wykres procesu Poissona
 plt.figure(figsize=(10, 5))
